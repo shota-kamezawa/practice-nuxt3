2a7a9efca59065831a961be16506c21d914d6f5b
shota-kamezawa <75684759+shota-kamezawa@users.noreply.github.com>
2023-03-29 18:11:19 +0900

    use cdn for vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css',
+          rel: 'stylesheet',
+        },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'crossorigin' as never,
+        },
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+          rel: 'stylesheet',
+        },
+      ],
+    },
+  },
+
   build: {
     transpile: ['vuetify'],
   },
