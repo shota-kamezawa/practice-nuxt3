b2fd49edc48793a02b2f81f6bad968152608dc7c
shota-kamezawa <75684759+shota-kamezawa@users.noreply.github.com>
2023-03-29 19:53:46 +0900

    add component: custom-loading-indicator

---
+++ src/components/custom-loading-indicator/custom-loading-indicator.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/**
+ * @see https://nuxt.com/docs/api/components/nuxt-loading-indicator/
+ */
+
+import { VOverlay } from 'vuetify/components/VOverlay';
+import { VProgressCircular } from 'vuetify/components/VProgressCircular';
+
+const props = defineProps<{ modelValue: boolean }>();
+</script>
+
+<template>
+  <VOverlay
+    content-class="w-100 h-100 d-flex justify-center align-center"
+    :model-value="props.modelValue"
+    persistent
+  >
+    <VProgressCircular color="white" indeterminate size="100" width="10" />
+  </VOverlay>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { VApp } from 'vuetify/components/VApp';
 import { VMain } from 'vuetify/components/VMain';
+import { useLoadingIndicator } from '@/stores/loading-indicator';
+
+import CustomLoadingIndicator from '@/components/custom-loading-indicator/custom-loading-indicator.vue';
+
+const loadingIndicatorStore = useLoadingIndicator();
 </script>
 
 <template>
+  <CustomLoadingIndicator v-model="loadingIndicatorStore.isLoading" />
   <VApp>
     <VMain>
       <NuxtPage />
