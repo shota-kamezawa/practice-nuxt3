c0237c317cdb40512e91e49df579eada478357f8
shota-kamezawa <75684759+shota-kamezawa@users.noreply.github.com>
2023-03-30 14:11:07 +0900

    add resolve.alias for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.join(__dirname, 'src'),
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+
   test: {
     globals: false,
   },
